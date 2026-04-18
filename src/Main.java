80d42c5731af9f311bc7c76989aa6198ea66a99a
ivoivkovich <ivoivkovich@gmail.com>
2026-04-18 21:06:29 +0300

    HW Method V1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 
 //Задача №1
 public static void printLeapYear (short yaer1) {
-    System.out.println(yaer1);
     short startYear = 1584;
     int everyFourthYear = yaer1 % 4;
     int everyFourHundredthYear = yaer1 % 400;
@@ -12,9 +11,54 @@ public static void printLeapYear (short yaer1) {
         System.out.println(yaer1 + " год не является високосным");
     }
 }
+//Задача №2
+public static void printVersionDevice (byte clientOS, short clientDeviceYear) {
 
+    if (clientDeviceYear < 2015 && clientOS == 1) {
+        System.out.println("Установите версию приложения для Android по ссылке" + " Установите облегченную версию приложения для Android по ссылке");
+    } else if (clientDeviceYear < 2015 && clientOS == 0) {
+        System.out.println("Установите версию приложения для iOS по ссылке" + " Установите облегченную версию приложения для IOS по ссылке");
+    } else if (clientDeviceYear >= 2015 && clientOS == 1) {
+        System.out.println("Установите версию приложения для Android по ссылке");
+    } else if (clientDeviceYear >= 2015 && clientOS == 0) {
+        System.out.println("Установите версию приложения для iOS по ссылке");
+    }
+}
+public static void printDelivery (byte deliveryDistance) {
+    byte day1 = 1;
+    byte day2 = 2;
+    byte day3 = 3;
+    if (deliveryDistance > 0 && deliveryDistance <= 20) {
+        System.out.println("Потребуется дней: " + day1);
+    } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+        System.out.println("Потребуется дней: " + day2);
+    } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+        System.out.println("Потребуется дней: " + day3);
+    } else {
+        System.out.println("Доставка не осуществляется");
+    }
+}
 void main() {
-    System.out.println("Задача №1");
-    short year = 2024;
-    printLeapYear(year);
+    {
+//Задача №1
+        System.out.println("Задача №1");
+        short year = 2024;
+        printLeapYear(year);
+//Задача №1
+    }
+    {
+//Задача №2
+        System.out.println("Задача №2");
+        byte clientOS = 0;
+        short clientDeviceYear = 2015;
+        printVersionDevice(clientOS, clientDeviceYear);
+//Задача №2
+    }
+    {
+//Задача №3
+        System.out.println("Задача №3");
+        byte deliveryDistance = 21;
+        printDelivery(deliveryDistance);
+//Задача №3
+    }
 }
